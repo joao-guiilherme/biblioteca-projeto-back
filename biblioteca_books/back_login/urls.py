362cb7ba7d01d29430a196c02ec3b03801fe7491
Wrose_05 <akashihiino@gmail.com>
2024-11-24 01:33:09 -0300

    adicionando passo a passo

--- biblioteca_books/back_login/urls.py
+++ biblioteca_books/back_login/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from .views import LoginView
-from .views import UserRegisterView
-from .views import LoginView, UserRegisterView
+from .views import LoginView
 
 from django.urls import path
-from .views import login_view, LoginView, UserRegisterView
+from .views import login_view, LoginView
 
 urlpatterns = [
     path('login/', login_view, name='login'), 
-    path('api/login/', LoginView.as_view(), name='login-api'),  
-    path('register/', UserRegisterView.as_view(), name='register'), 
-    path('livros/', views.livros_view, name='livros'),
+    path('api/login/', LoginView.as_view(), name='login-api')
 ]
