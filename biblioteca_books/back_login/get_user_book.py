42d43b373029de24ee7f0e0bf1f491c8ee521f32
wrose-05 <akashihiino@gmail.com>
2024-11-26 15:19:19 -0300

    Ajustando esse teste de orm

--- biblioteca_books/back_login/get_user_book.py
+++ biblioteca_books/back_login/get_user_book.py
@@ -1,14 +1,14 @@
-
+# isso aqui sou eu fazendo um teste 
 
 
 from back_login.models import User, Books
-#esse aqui é uma orm, para retornar dados do banco
-# Obtém todos os livros favoritos de um usuário
-user = User.objects.get(user_id=1)  # aqui ele busca através do id, nesse exemplo, o id é 1
 
-# Acesse os livros favoritos do usuário
-livros_favoritos = user.user_user_livros_favoritos.all()
-
-# Agora você pode acessar o id_user e id_books
-for books in livros_favoritos:
-    print(f"User ID: {user.id_user}, Book ID: {livros_favoritos.id_books}")
+# Este código faz uso de ORM do Django, ou seja, ele utiliza a abstração do banco de dados para trabalhar com os modelos definidos em models.py.
+# Obtém um usuário pelo ID
+user = User.objects.get(id_user=1)  # Aqui, o código busca o usuário com id_user = 1
+# O relacionamento muitos para muitos é acessado através do modelo intermediário 
+livros_favoritos = user.livros_favoritos.all() 
+# acessar os livros favoritos do usuário
+for livro in livros_favoritos:
+    # Para cada livro, imprimimos o ID do usuário e o ID do livro
+    print(f"User ID: {user.id_user}, Book ID: {livro.id_books}")  # Aqui é necessário usar 'livro.id_books' 
